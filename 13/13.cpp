ecd3a2829f6ae58812c1c16971531a2fdcb20da3
Vadim <gwinbladewaswrong@gmail.com>
2019-02-09 12:34:20 +0300

    Vadim: 13,15 done!

---
+++ 13/13.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+struct Matrix
+{
+	double matrix[100][100];
+	int M;
+};
+
+void read(Matrix& a)
+{
+	do
+	{
+		std::cout << "Enter M: ";
+		std::cin >> a.M;
+		if (std::cin)
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+
+	} while (a.M <= 0 || a.M > 100);
+
+	for (int i = 0; i < a.M; ++i)
+	{
+		for (int j = 0; j < a.M; ++j)
+		{
+			std::cin >> a.matrix[i][j];
+		}
+	}
+}
+void result(Matrix& a)
+{
+	std::cout << "\n\n";
+	for (int i = 0; i < a.M; ++i)
+	{
+		for (int j = 0; j < a.M; ++j)
+		{
+			std::cout << a.matrix[a.M - j - 1][i] << ' ';
+		}
+		std::cout << '\n';
+	}
+}
+
+int main()
+{
+	Matrix matrix;
+
+	read(matrix);
+	result(matrix);
+
+	system("pause");
+	return 0;
+}
---
+++ 15/15.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+struct Matrix
+{
+	double matrix[100][100];
+	int row;
+	int column;
+};
+
+void read(Matrix& a)
+{
+	do
+	{
+		std::cout << "Enter M: ";
+		std::cin >> a.row;
+		if (std::cin)
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+
+	} while (a.row <= 0 || a.row > 100);
+
+	do
+	{
+		std::cout << "Enter N: ";
+		std::cin >> a.column;
+		if (std::cin)
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+
+	} while (a.column <= 0 || a.column > 100);
+
+	for (int i = 0; i < a.row; ++i)
+	{
+		for (int j = 0; j < a.column; ++j)
+		{
+			std::cin >> a.matrix[i][j];
+		}
+	}
+}
+
+void result(Matrix& a)
+{
+	std::cout << "\n\n";
+
+	// Предполагается, что нумерация начиется с нуля.
+	for (int i = 0; i < a.row; i += 2)
+	{
+		for (int j = 0; j < a.column; ++j)
+		{
+			std::cout << a.matrix[i][j] << ' ';
+		}
+		std::cout << '\n';
+	}
+}
+
+int main(void)
+{
+	Matrix matrix;
+	read(matrix);
+	result(matrix);
+
+	system("pause");
+	return 0;
+}
