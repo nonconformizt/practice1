82b241442ed0c40f63073485e007c378f9df8226
Nonconformizt <nonconformizt@gmail.com>
2019-02-09 15:05:29 +0300

    Nonconformizt : task 14 final

--- 14/14.cpp
+++ 14/14.cpp
@@ -1,67 +1,65 @@
 #include <iostream>
 #include <time.h>
-#define N 5 
-#define M 3
-void output(int a[M][N])
+#define _CRT_SECURE_NO_WARNINGS
+#define SIZE 100
+
+void output(int a[SIZE][SIZE], int m, int n)
 {
-	for (int i = 0; i < M; i++)
-	{
-		for (int j = 0; j < N; j++)
-			printf("a[%d;%d]= %d\t", i, j, a[i][j]);
-		printf("\n");
-	}
+    printf("=== MATRIX ===\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%d\t", a[i][j]);
+        printf("\n");
+    }
 }
-void key(int a[M][N])
+
+void key( int a[SIZE][SIZE], int* m, int *n )
 {
-	for (int i = 0; i < M; i++)
-		for (int j = 0; j < N; j++)
-		{
-			printf(" a[%d;%d]= ", i, j);
-			scanf_s("%d", &a[i][j]);
-		}
+    printf("Insert N\n");
+    do {
+        scanf("%d", n);
+    } while ( *n < 1 || *n > SIZE );
 
+    printf("Insert M\n");
+    do {
+        scanf("%d", m);
+    } while ( *m < 1 || *m > SIZE );
 
+    for (int i = 0; i < *m; i++)
+        for (int j = 0; j < *n; j++)
+        {
+            printf(" a[%d;%d]= ", i, j);
+            scanf("%d", &a[i][j]);
+        }
 }
-/*void rand(int a[M][N])
+
+void downup(int a[SIZE][SIZE], int m, int n)
 {
-	int A, B;
-	srand(time(NULL));
-	do
-	{
-		printf("¬ведите промежуток [a,b] ");
-		scanf_s("%d%d", &A, &B);
-	} while (A > B);
-	for (int i = 0; i < M; i++)
-		for (int j = 0; j < N; j++)
-			a[i][j] = rand() % (B - A + 1) + A;
-	output(a);
+    int t;
+    for (int j = 0; j < n; j++)
+    {
+        if (j % 2 != 0)
 
-
-}*/
-void downup(int a[M][N])
-{
-	int t;
-		for (int j = 0; j < N; j++)
-	{
-		if (j % 2 != 0)
-
-			for (int i = 0; i < M/2; i++)
-			{
-			t = a[i][j];
-			a[i][j] = a[M - i-1][j];
-			a[M - i-1][j] = t;
-		    }
-	}
-	output(a);
+            for (int i = 0; i < m/2; i++)
+            {
+                t = a[i][j];
+                a[i][j] = a[m - i-1][j];
+                a[m - i-1][j] = t;
+            }
+    }
 }
-void main()
+
+int main()
 {
-	setlocale(LC_ALL, "rus");
-	int a[M][N];
-	//rand(a);
-	key(a);
-	printf("\n"
-		"\n");
-	downup(a);
-	system("pause");
+    setlocale(LC_ALL, "rus");
+    int a[SIZE][SIZE], m, n;
+
+    key(a, &m, &n);
+    output(a, m, n);
+    printf("\n\n");
+    downup(a, m, n);
+    output(a, m, n);
+
+    getchar();
 }
